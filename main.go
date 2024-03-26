9a023d49e818daddf3224fd39fea6eaa778561a9
DAECHUNG YOO <dcyoo777@gmail.com>
2024-03-26 18:46:04 +0900

    add cors;

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"example/router"
 	"github.com/gin-contrib/cors"
+	"time"
 )
 
 func main() {
 	r := router.SetupRouter()
-	r.Use(cors.Default())
+	r.Use(cors.New(
+		cors.Config{
+			AllowOrigins: []string{"http://localhost:3000"},
+			AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+			MaxAge:       12 * time.Hour,
+		}))
 	r.Run(":8080")
 }
